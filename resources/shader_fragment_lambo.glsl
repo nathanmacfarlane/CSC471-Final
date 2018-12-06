f33f4aec22e32544958dac9d28a3d49963d043fb
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2018-12-05 20:58:43 -0800

    dark gradient on car

--- resources/shader_fragment_lambo.glsl
+++ resources/shader_fragment_lambo.glsl
@@ -12,11 +12,16 @@ uniform vec3 campos;
 void main()
 {
     vec3 n = normalize(vertex_normal);
-    vec3 lp=vec3(15,-20,-100);
+    vec3 lp=vec3(15, -20, -100);
     vec3 ld = normalize(vertex_pos - lp);
     float diffuse = dot(n,ld);
 
     color = texture(texL, vertex_tex);
     color *= diffuse * 2;
 
+    color *= ((vertex_pos.z + 1.001)/2);
+
+    if (isBraking > 0.5 && vertex_pos.z < -0.9) {
+        color.r += 0.5;
+    }
 }
